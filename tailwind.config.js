161859b6e9ab8995538e77f0544319e6a84a9d74
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2023-07-11 18:16:23 +0700

    Tailwind colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,19 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      colors: {
+        vnm: '#0213b0',
+        main: '#fffff1',
+        white: '#fffff1',
+        note: '#8c8c8c',
+      },
       fontFamily: {
         'sans-display-bold': ['VNM Sans Display Bold', ...defaultTheme.fontFamily.sans],
-        'sans-std': ['VNM Sans Std', ...defaultTheme.fontFamily.sans],
         'sans-display': ['VNM Sans Display', ...defaultTheme.fontFamily.sans],
+        'sans-std': ['VNM Sans Std', ...defaultTheme.fontFamily.sans],
         'inter': ['Inter', ...defaultTheme.fontFamily.sans],
       },
     },
