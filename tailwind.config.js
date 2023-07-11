c1790a10d603b89f18f02609fcbcf2425341153a
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2023-07-11 15:51:26 +0700

    Add fonts to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans-display-bold': ['VNM Sans Display Bold', ...defaultTheme.fontFamily.sans],
+        'sans-std': ['VNM Sans Std', ...defaultTheme.fontFamily.sans],
+        'sans-display': ['VNM Sans Display', ...defaultTheme.fontFamily.sans],
+        'inter': ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
