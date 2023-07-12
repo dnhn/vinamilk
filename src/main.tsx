4bf38c6185398dedc02d3dbac043fe440f69513a
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2023-07-13 06:21:08 +0700

    NPM

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ReactModal from 'react-modal';
 
 import App from './App.tsx';
 
 import './scss/index.scss';
-
-ReactModal.setAppElement('#root');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
