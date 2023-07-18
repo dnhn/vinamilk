65ff0d7686bb8a0c29fd97f48fd1bf4f359e459a
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2023-07-18 15:47:28 +0700

    Configure build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('react') || id.includes('scheduler')) {
+            return 'react';
+          } else if (id.includes('html-to-image')) {
+            return 'html-to-image';
+          } else {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+  plugins: [react(), splitVendorChunkPlugin()],
+});
