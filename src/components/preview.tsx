05f09b6cfb54be62936eaffbe5060827fedede7e
nhan.dev <hey@nhan.dev>
2025-05-06 14:45:39 +0700

    Fix condition

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -70,7 +70,7 @@ export default function Preview({
             {data.name && (
               <div className="-mt-1 flex justify-between">
                 <span className="font-sans-display-bold text-[1.3125rem]">
-                  {data.est ?? (data.year && 'EST')}
+                  {data.est || (data.year && 'EST')}
                 </span>
                 <span className="font-sans-display-bold text-[1.3125rem]">
                   {data.year}
